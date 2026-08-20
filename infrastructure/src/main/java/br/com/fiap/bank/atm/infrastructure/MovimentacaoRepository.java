bd4df0933936237e6ad4e917dd758c1ce461e31c
Prof.  Eduardo Ramos <profeduardo.ramos@fiap.com.br>
2026-08-20 12:08:24 +0000

    2ESPG | 2026-8-20 | Stream API | Optional

--- infrastructure/src/main/java/br/com/fiap/bank/atm/infrastructure/MovimentacaoRepository.java
+++ infrastructure/src/main/java/br/com/fiap/bank/atm/infrastructure/MovimentacaoRepository.java
@@ -3,10 +3,16 @@ package br.com.fiap.bank.atm.infrastructure;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
+
 import br.com.fiap.bank.atm.domain.interfaces.ATMRepository;
+import br.com.fiap.bank.atm.domain.Conta;
 import br.com.fiap.bank.atm.domain.Movimentacao;
+import br.com.fiap.bank.atm.domain.TipoMovimentacao;
 
 public class MovimentacaoRepository implements ATMRepository<Movimentacao> {
      // Lista insegura atual do FIAP Bank
@@ -17,13 +23,12 @@ public class MovimentacaoRepository implements ATMRepository<Movimentacao> {
     }
 
     // O jeito antigo (Java 7-): Risco altíssimo de NullPointerException
-    public Movimentacao buscarPorId(UUID id) {
-        for (Movimentacao m : movimentacoes) {
-            if (m.getId().equals(id)) {
-                return m;
-            }
-        }
-        return null; // A bomba relógio!
+    public Optional<Movimentacao> buscarPorId(UUID id) {
+        return 
+            movimentacoes.stream()
+                .filter(movimentacao -> movimentacao.getId().equals(id))
+                .findFirst()
+        ;
     }
 
     public void remover(UUID id) {
@@ -34,4 +39,27 @@ public class MovimentacaoRepository implements ATMRepository<Movimentacao> {
         // Convertendo o Set de volta para List para visualização
         return new ArrayList<>(movimentacoes);
     }
+
+    public List<String> gerarRecibosSimples() {
+        return movimentacoes.stream()
+            .map(movimentacao -> "RECIBO | " + movimentacao.getTipo() + " - Valor: R$ " + movimentacao.getValor().getValor())
+            .collect(Collectors.toList())
+        ;
+    }
+
+    public List<Movimentacao> obterSaques() {
+        return movimentacoes.stream()
+            .filter(movimentacao -> TipoMovimentacao.SAQUE.equals(movimentacao.getTipo()))
+            .collect(Collectors.toList())
+        ;
+    }
+
+    // Agrupa todas as movimentacoes separadas por conta utilizando a Entidade inteira como chave
+    public Map<TipoMovimentacao, List<Movimentacao>> gerarExtratoAgrupado() {
+        return movimentacoes.stream()
+                .collect(Collectors.groupingBy(Movimentacao::getTipo))
+            ;
+    }
+
+
 }
