2c6b5aee0997e3ec4ecfc9cf0563843845e24ef4
prof-eduardo-ramos <profeduardo.ramos@fiap.com.br>
2026-08-21 10:51:25 -0300

    feat: add sqlite-jdbc driver and junit-jupiter-api dependencies to infrastructure module

--- infrastructure/src/main/java/br/com/fiap/bank/atm/infrastructure/repository/MovimentacaoRepository.java
+++ infrastructure/src/main/java/br/com/fiap/bank/atm/infrastructure/repository/MovimentacaoRepository.java
@@ -1,4 +1,4 @@
-package br.com.fiap.bank.atm.infrastructure;
+package br.com.fiap.bank.atm.infrastructure.repository;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -14,7 +14,7 @@ import br.com.fiap.bank.atm.domain.Movimentacao;
 import br.com.fiap.bank.atm.domain.TipoMovimentacao;
 
 public class MovimentacaoRepository implements ATMRepository<Movimentacao> {
-     // Lista insegura atual do FIAP Bank
+    // Lista insegura atual do FIAP Bank
     private Set<Movimentacao> movimentacoes = new HashSet<>();
 
     public void adicionar(Movimentacao entidade) {
@@ -24,9 +24,8 @@ public class MovimentacaoRepository implements ATMRepository<Movimentacao> {
     // O jeito antigo (Java 7-): Risco altíssimo de NullPointerException
     public Optional<Movimentacao> buscarPorId(UUID id) {
         return movimentacoes.stream()
-            .filter(movimentacao -> movimentacao.getId().equals(id))
-            .findFirst()
-        ;
+                .filter(movimentacao -> movimentacao.getId().equals(id))
+                .findFirst();
     }
 
     public void remover(UUID id) {
@@ -40,19 +39,18 @@ public class MovimentacaoRepository implements ATMRepository<Movimentacao> {
 
     public List<String> gerarRecibosSimples() {
         return movimentacoes.stream()
-            // O map() extrai informações, navegando pelos Value Objects (Dinheiro)
-            .filter(movimentacao -> movimentacao.getTipo().equals(TipoMovimentacao.SAQUE))
-            .map(movimentacao -> "RECIBO | " + movimentacao.getTipo() + " - Valor: R$ " + movimentacao.getValor().getValor())
-            .collect(Collectors.toList())
-        ;
+                // O map() extrai informações, navegando pelos Value Objects (Dinheiro)
+                .filter(movimentacao -> movimentacao.getTipo().equals(TipoMovimentacao.SAQUE))
+                .map(movimentacao -> "RECIBO | " + movimentacao.getTipo() + " - Valor: R$ "
+                        + movimentacao.getValor().getValor())
+                .collect(Collectors.toList());
     }
 
     public BigDecimal calcularTotalSaques() {
         return movimentacoes.stream()
-            .filter(movimentacao -> movimentacao.getTipo().equals(TipoMovimentacao.SAQUE))
-            .map(movimentacao -> movimentacao.getValor().getValor())
-            .reduce(BigDecimal.ZERO, BigDecimal::add)
-        ;
+                .filter(movimentacao -> movimentacao.getTipo().equals(TipoMovimentacao.SAQUE))
+                .map(movimentacao -> movimentacao.getValor().getValor())
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 
 }
