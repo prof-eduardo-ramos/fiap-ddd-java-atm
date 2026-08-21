27d4109f224682aac13ce24049f9fd6043af6ae7
prof-eduardo-ramos <profeduardo.ramos@fiap.com.br>
2026-08-21 12:07:38 -0300

    feat: implement JDBC persistence methods for Conta entity and add integration tests

--- infrastructure/src/main/java/br/com/fiap/bank/atm/infrastructure/repository/jdbc/ContaRepositoryJdbcImpl.java
+++ infrastructure/src/main/java/br/com/fiap/bank/atm/infrastructure/repository/jdbc/ContaRepositoryJdbcImpl.java
@@ -1,30 +1,65 @@
 package br.com.fiap.bank.atm.infrastructure.repository.jdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import br.com.fiap.bank.atm.domain.Cliente;
 import br.com.fiap.bank.atm.domain.Conta;
+import br.com.fiap.bank.atm.domain.ContaAcesso;
+import br.com.fiap.bank.atm.domain.ContaCorrente;
+import br.com.fiap.bank.atm.domain.Dinheiro;
 import br.com.fiap.bank.atm.domain.interfaces.ATMRepository;
+import br.com.fiap.bank.atm.infrastructure.database.DatabaseConnectionFactory;
 
 public class ContaRepositoryJdbcImpl implements ATMRepository<Conta> {
 
     @Override
-    public void adicionar(Conta entidade) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'adicionar'");
+    public void adicionar(Conta conta) {
+        String sql = "INSERT INTO tb_conta (id, cliente_id, agencia, numero, saldo, status) VALUES " +
+                "('" + conta.getId() + "', '" + conta.getCliente().getId() + "', '" + conta.getAgencia() + "', '"
+                + conta.getNumero() + "', " + conta.getSaldo().getValor().doubleValue() + ", '"
+                + conta.getStatus().name() + "')";
+
+        try (Connection conn = DatabaseConnectionFactory.getConnection(); Statement stmt = conn.createStatement()) {
+            stmt.executeUpdate(sql);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao adicionar a conta", e);
+        }
     }
 
     @Override
-    public void atualizar(Conta entidade) {
+    public void atualizar(Conta conta) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'atualizar'");
     }
 
     @Override
     public Optional<Conta> buscarPorId(UUID id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscarPorId'");
+        String sql = "SELECT * FROM tb_conta co, tb_cliente cl, tb_conta_acesso ca WHERE cl.id = co.cliente_id AND co.id = ca.conta_id AND co.id = '"
+                + id.toString() + "'";
+
+        try (Connection conn = DatabaseConnectionFactory.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+
+            if (rs.next()) {
+                Cliente cliente = new Cliente(rs.getString("nome"));
+                ContaAcesso contaAcesso = new ContaAcesso(rs.getString("senha"));
+                Conta conta = new ContaCorrente(
+                        cliente,
+                        contaAcesso,
+                        new Dinheiro(rs.getBigDecimal("saldo")));
+                return Optional.of(conta);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro na busca da conta", e);
+        }
+        return Optional.empty();
     }
 
     @Override
--- infrastructure/src/test/java/br/com/fiap/bank/atm/infrastructure/ContaRepositoryJdbcImplTest.java
+++ infrastructure/src/test/java/br/com/fiap/bank/atm/infrastructure/ContaRepositoryJdbcImplTest.java
@@ -2,10 +2,19 @@ package br.com.fiap.bank.atm.infrastructure;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import br.com.fiap.bank.atm.domain.Cliente;
+import br.com.fiap.bank.atm.domain.Conta;
+import br.com.fiap.bank.atm.domain.ContaAcesso;
+import br.com.fiap.bank.atm.domain.ContaCorrente;
+import br.com.fiap.bank.atm.domain.Dinheiro;
 import br.com.fiap.bank.atm.infrastructure.database.DatabaseSetup;
+import br.com.fiap.bank.atm.infrastructure.repository.jdbc.ContaRepositoryJdbcImpl;
 
 public class ContaRepositoryJdbcImplTest {
     @BeforeAll
@@ -15,7 +24,17 @@ public class ContaRepositoryJdbcImplTest {
     }
 
     @Test
-    public void deveSalvarEBuscarContaNoBancoDeDados() {
-        assertTrue(false);
+    public void deveSalvaContaNoBancoDeDados() {
+        Cliente cliente = new Cliente("Teste nome cliente");
+        ContaAcesso contaAcesso = new ContaAcesso("1234");
+        Dinheiro saldo = new Dinheiro(new BigDecimal("1000.00"));
+        Conta conta = new ContaCorrente(cliente, contaAcesso, saldo);
+
+        ContaRepositoryJdbcImpl repository = new ContaRepositoryJdbcImpl();
+        repository.adicionar(conta);
+
+        Optional<Conta> contaEncontrada = repository.buscarPorId(conta.getId());
+
+        assertTrue(contaEncontrada.isPresent());
     }
 }
