8b6966ef52f8bb876f47c0a0b65f9d5da37ee940
prof-eduardo-ramos <profeduardo.ramos@fiap.com.br>
2026-08-28 10:11:49 -0300

    feat: implement remover and buscarTodas methods in ContaRepositoryJdbcImpl

--- infrastructure/src/main/java/br/com/fiap/bank/atm/infrastructure/repository/jdbc/ContaRepositoryJdbcImpl.java
+++ infrastructure/src/main/java/br/com/fiap/bank/atm/infrastructure/repository/jdbc/ContaRepositoryJdbcImpl.java
@@ -4,7 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -23,7 +24,8 @@ public class ContaRepositoryJdbcImpl implements ATMRepository<Conta> {
     public void adicionar(Conta conta) {
         String sql = "INSERT INTO tb_conta (id, cliente_id, agencia, numero, saldo, status) VALUES (?, ?. ?, ?, ?, ?)";
 
-        try (Connection conn = DatabaseConnectionFactory.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
+        try (Connection conn = DatabaseConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, conta.getId().toString());
             stmt.setString(2, conta.getCliente().getId().toString());
@@ -43,7 +45,7 @@ public class ContaRepositoryJdbcImpl implements ATMRepository<Conta> {
         String sql = "UPDATE tb_conta SET saldo = ?, status = ? WHERE numero = ?";
 
         try (Connection conn = DatabaseConnectionFactory.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             // Os parâmetros são definidos explicitamente com seus tipos
             stmt.setBigDecimal(1, conta.getSaldo().getValor());
@@ -60,8 +62,9 @@ public class ContaRepositoryJdbcImpl implements ATMRepository<Conta> {
     public Optional<Conta> buscarPorId(UUID id) {
         String sql = "SELECT * FROM tb_conta co, tb_cliente cl, tb_conta_acesso ca WHERE cl.id = co.cliente_id AND co.id = ca.conta_id AND co.id = ?";
 
-        try (Connection conn = DatabaseConnectionFactory.getConnection(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-            
+        try (Connection conn = DatabaseConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
             stmt.setString(1, id.toString());
 
             ResultSet rs = stmt.executeQuery();
@@ -83,14 +86,39 @@ public class ContaRepositoryJdbcImpl implements ATMRepository<Conta> {
 
     @Override
     public void remover(UUID id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'remover'");
+        String sql = "DELETE FROM tb_conta WHERE id = ?";
+
+        try (Connection conn = DatabaseConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, id.toString());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao remover a conta", e);
+        }
     }
 
     @Override
     public List<Conta> buscarTodas() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscarTodas'");
+        String sql = "SELECT * FROM tb_conta co, tb_cliente cl, tb_conta_acesso ca WHERE cl.id = co.cliente_id AND co.id = ca.conta_id";
+
+        try (Connection conn = DatabaseConnectionFactory.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+            List<Conta> contas = new ArrayList<>();
+
+            while (rs.next()) {
+                Cliente cliente = new Cliente(rs.getString("nome"));
+                ContaAcesso contaAcesso = new ContaAcesso(rs.getString("senha"));
+                contas.add(new ContaCorrente(
+                        cliente,
+                        contaAcesso,
+                        new Dinheiro(rs.getBigDecimal("saldo"))));
+                return contas;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro na busca da conta", e);
+        }
+        return Collections.emptyList();
     }
 
 }
