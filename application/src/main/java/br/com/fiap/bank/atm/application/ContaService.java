3b93770f12f92da96c90415c66cc98f06c1ef62e
prof-eduardo-ramos <profeduardo.ramos@fiap.com.br>
2026-09-11 11:07:52 -0300

    refactor: refactor ContaService to use constructor injection, update DTOs, and simplify business logic methods

--- application/src/main/java/br/com/fiap/bank/atm/application/ContaService.java
+++ application/src/main/java/br/com/fiap/bank/atm/application/ContaService.java
@@ -1,127 +1,87 @@
 package br.com.fiap.bank.atm.application;
 
-import br.com.fiap.bank.atm.application.dto.CadastrarContaDTO;
-import br.com.fiap.bank.atm.application.dto.ContaDTO;
-import br.com.fiap.bank.atm.application.dto.MovimentacaoDTO;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+import br.com.fiap.bank.atm.application.dto.ContaRequestDTO;
+import br.com.fiap.bank.atm.application.dto.ContaResponseDTO;
+import br.com.fiap.bank.atm.application.dto.MovimentacaoResponseDTO;
 import br.com.fiap.bank.atm.domain.Cliente;
 import br.com.fiap.bank.atm.domain.Conta;
 import br.com.fiap.bank.atm.domain.ContaAcesso;
 import br.com.fiap.bank.atm.domain.Dinheiro;
 import br.com.fiap.bank.atm.domain.Movimentacao;
-import br.com.fiap.bank.atm.domain.interfaces.ClienteRepository;
-import br.com.fiap.bank.atm.domain.interfaces.ContaAcessoRepository;
 import br.com.fiap.bank.atm.domain.interfaces.ContaRepository;
 import br.com.fiap.bank.atm.domain.interfaces.MovimentacaoRepository;
-import br.com.fiap.bank.atm.infrastructure.repository.jdbc.ClienteRepositoryJdbcImpl;
-import br.com.fiap.bank.atm.infrastructure.repository.jdbc.ContaAcessoRepositoryJdbcImpl;
-import br.com.fiap.bank.atm.infrastructure.repository.jdbc.ContaRepositoryJdbcImpl;
-import br.com.fiap.bank.atm.infrastructure.repository.jdbc.MovimentacaoRepositoryJdbcImpl;
-
-import java.math.BigDecimal;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 public class ContaService {
 
-    private ContaRepository contaRepository;
-    private ClienteRepository clienteRepository;
-    private ContaAcessoRepository contaAcessoRepository;
-    private MovimentacaoRepository movimentacaoRepository;
+    private final ContaRepository contaRepository;
+    private final MovimentacaoRepository movimentacaoRepository;
 
-    public ContaService() {
-        this.contaRepository = new ContaRepositoryJdbcImpl();
-        this.clienteRepository = new ClienteRepositoryJdbcImpl();
-        this.contaAcessoRepository = new ContaAcessoRepositoryJdbcImpl();
-        this.movimentacaoRepository = new MovimentacaoRepositoryJdbcImpl();
+    public ContaService(ContaRepository contaRepository, MovimentacaoRepository movimentacaoRepository) {
+        this.contaRepository = contaRepository;
+        this.movimentacaoRepository = movimentacaoRepository;
     }
 
-    public void realizarDeposito(UUID idConta, BigDecimal valor) {
-        Optional<Conta> optionalConta = contaRepository.buscarPorId(idConta);
+    public ContaResponseDTO consultarConta(UUID id) {
+        Conta conta = obterContaPorId(id);
 
-        if (optionalConta.isPresent()) {
-            Conta conta = optionalConta.get();
-            conta.realizarDeposito(new Dinheiro(valor));
-            contaRepository.atualizar(conta);
-        }
+        return new ContaResponseDTO(
+                conta.getId(),
+                conta.getCliente().getNomeCompleto(),
+                conta.getCliente().getCpf(),
+                conta.getNumero(),
+                conta.getAgencia(),
+                conta.getSaldo().getValor());
+
     }
 
-    public void realizarSaque(UUID idConta, BigDecimal valor) {
-        Optional<Conta> optionalConta = contaRepository.buscarPorId(idConta);
-
-        if (optionalConta.isPresent()) {
-            Conta conta = optionalConta.get();
-            conta.realizarSaque(new Dinheiro(valor));
-            contaRepository.atualizar(conta);
-        }
-    }
-
-    public BigDecimal obterSaldo(UUID idConta) {
-        Optional<Conta> optionalConta = contaRepository.buscarPorId(idConta);
-
-        if (optionalConta.isPresent()) {
-            Conta conta = optionalConta.get();
-            return conta.getSaldo().getValor();
-        }
-
-        return BigDecimal.ZERO;
-    }
-
-    public List<MovimentacaoDTO> obterMovimentacoes(UUID idConta) {
-        List<Movimentacao> movimentacoes = movimentacaoRepository.buscarPorIdConta(idConta);
-        return movimentacoes.stream().map(m -> new MovimentacaoDTO(
-                idConta,
-                m.getDataHora(),
-                m.getValor().getValor(),
-                m.getTipo().name()))
-                .collect(Collectors.toList());
-    }
-
-    public String obterNomeCliente(UUID idConta) {
-        Optional<Conta> optionalConta = contaRepository.buscarPorId(idConta);
-
-        if (optionalConta.isPresent()) {
-            Conta conta = optionalConta.get();
-            return conta.getCliente().obterPrimeiroNome();
-        }
-
-        return "";
-    }
-
-    public UUID salvarConta(CadastrarContaDTO dto) {
+    public ContaResponseDTO cadastrarNovaConta(ContaRequestDTO dto) {
         Cliente cliente = new Cliente(dto.nomeCliente(), dto.cpfCliente());
         ContaAcesso contaAcesso = new ContaAcesso(dto.senha());
-        Dinheiro saldo = new Dinheiro(dto.saldoInicial());
-        Conta conta = ContaFactory.getInstance().criarContaCorrente(dto.numero(), dto.agencia(), cliente, contaAcesso,
-                saldo);
+        Conta novaConta = ContaFactory.getInstance().criarContaCorrente(dto.numero(), dto.agencia(), cliente,
+                contaAcesso, new Dinheiro(dto.saldoInicial()));
 
-        clienteRepository.adicionar(cliente);
-        contaRepository.adicionar(conta);
-        contaAcessoRepository.adicionar(contaAcesso);
+        contaRepository.adicionar(novaConta);
 
-        return conta.getId();
+        return new ContaResponseDTO(
+                novaConta.getId(),
+                novaConta.getCliente().getNomeCompleto(),
+                novaConta.getCliente().getCpf(),
+                novaConta.getNumero(),
+                novaConta.getAgencia(),
+                novaConta.getSaldo().getValor());
+
     }
 
-    public Optional<ContaDTO> buscarContaPorId(UUID idConta) {
-        Optional<Conta> optionalConta = contaRepository.buscarPorId(idConta);
+    public List<MovimentacaoResponseDTO> consultarMovimentacoes(UUID id) {
+        List<Movimentacao> movimentacoes = movimentacaoRepository.buscarPorIdConta(id);
 
-        if (optionalConta.isPresent()) {
-            Conta conta = optionalConta.get();
-            return Optional.of(new ContaDTO(
-                    conta.getId(),
-                    conta.getNumero(),
-                    conta.getAgencia(),
-                    conta.getSaldo().getValor(),
-                    conta.getTaxa(),
-                    conta.getStatus().name(),
-                    conta.getDataAbertura(),
-                    conta.getCliente().obterPrimeiroNome(),
-                    conta.getCliente().obterPrimeiroNome(),
-                    conta.getContaAcesso().getSenha()));
-        }
-
-        return Optional.empty();
+        return movimentacoes.stream()
+                .map(m -> new MovimentacaoResponseDTO(m.getTipo().name(), m.getValor().getValor(), m.getDataHora()))
+                .toList();
     }
 
+    public void realizarSaque(UUID id, BigDecimal valor) {
+        Conta conta = obterContaPorId(id);
+        conta.realizarSaque(new Dinheiro(valor));
+        contaRepository.atualizar(conta);
+    }
+
+    public void realizarDeposito(UUID id, BigDecimal valor) {
+        Conta conta = obterContaPorId(id);
+        conta.realizarDeposito(new Dinheiro(valor));
+        contaRepository.atualizar(conta);
+    }
+
+    public void cancelarConta(UUID id) {
+        contaRepository.remover(id);
+    }
+
+    private Conta obterContaPorId(UUID id) {
+        return contaRepository.buscarPorId(id)
+                .orElseThrow(() -> new IllegalArgumentException("Conta não encontrada."));
+    }
 }
