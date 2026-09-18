204d649849e21591fdea873680703bbaec4f6ace
eduardo-ramos-ntconsult <eduardo.ramos@ntconsult.com.br>
2026-09-18 10:39:22 -0300

    refactor: change BaseEntity from Entity to MappedSuperclass

--- domain/src/main/java/br/com/fiap/bank/atm/domain/BaseEntity.java
+++ domain/src/main/java/br/com/fiap/bank/atm/domain/BaseEntity.java
@@ -4,19 +4,16 @@ import java.time.LocalDate;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
+import jakarta.persistence.MappedSuperclass;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @EqualsAndHashCode(of = "id")
-@Entity
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@MappedSuperclass
 public abstract class BaseEntity {
 
     @Id
